260e5cb2f50f85b1350a1e52a7c6871f4aff4449
matthewlazewski <matthew.lazewski@gmail.com>
2020-06-02 15:42:10 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+    (1, 'Mr. Chocolate', 4, 'male', 'brown', 'calm', true ),
+    (2, 'Rowdy', 4, 'male', 'brown', 'calm', true),
+    (3, 'Tabitha', 4, 'male', 'brown', 'calm', true),
+    (4, 'Sergeant Brown', 4, 'male', 'brown', 'calm', true),
+    (5, 'Melissa',  4, 'male', 'brown', 'calm', true),
+    (6, 'Grinch', 4, 'male', 'brown', 'calm', true),
+    (7, 'Wendy', 4, 'male', 'brown', 'calm', true),
+    (8, null, 4, 'male', 'brown', 'calm', true);
